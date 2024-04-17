b891333d50e160efb6946f6ad9fff25bd8ed9e2f
SurgicalCoder <me@surgicalcoder.com>
2024-04-17 21:18:25 +0100

    Unneeded projects

--- src/Audit.WebApi/BodyContent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Audit.WebApi
-{
-    public class BodyContent
-    {
-        public string Type { get; set; }
-        public long? Length { get; set; }
-        public object Value { get; set; }
-    }
-}
